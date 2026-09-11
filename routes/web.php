fc97ea5eddb021d2b177937d3e63795856b625bc
nabila kayla rafa <nabilakayla.005@gmail.com>
2026-09-11 09:39:58 +0700

    feat: srs4 - implementasi ui dan fungsi CRUD manajemen pengguna admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route untuk Manajemen Pengguna
+Route::prefix('admin')->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 });
